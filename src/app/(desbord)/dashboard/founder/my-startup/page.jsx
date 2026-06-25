d61d4db51081e3ee16560f320770263f6f6f2118
hmdshahdat501-png <hmdshahdat501@gmail.comm>
2026-06-25 17:35:27 +0600

    add imge bb

--- src/app/(desbord)/dashboard/founder/my-startup/page.jsx
+++ src/app/(desbord)/dashboard/founder/my-startup/page.jsx
@@ -20,7 +20,7 @@ useEffect(() => {
     .then((res) => setData(res))
     .catch((err) => console.error(err));
 }, [email, token]);
-
+  
     
     const onSubmit = async (e) => {
     e.preventDefault();
