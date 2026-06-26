0a39fbf31161bef3b07c95830cb6655f1b2b4997
hmdshahdat501-png <hmdshahdat501@gmail.comm>
2026-06-26 21:26:42 +0600

    add blick page

--- src/app/(main)/opportunitiess/[id]/page.jsx
+++ src/app/(main)/opportunitiess/[id]/page.jsx
@@ -2,18 +2,25 @@ import OporsontiyDetles from "@/Component/OporsontiyDetles";
 import { auth } from "@/lib/auth";
 import { getApplicantByOpportunity, getDetlesOpportunities } from "@/lib/data";
 import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 const OpportunityDetailsPage = async ({ params }) => {
-    const { id } = await params;
+
+  const { id } = await params;
   const job = await getDetlesOpportunities(id);
   const session = await auth.api.getSession({
     headers: await headers(),
   });
   const idt = session?.user?.id;
-   const token = session.session.token
+  const token = session.session.token
+  const userStatus = session.user.status;
+  if (userStatus !== "Unblock") {
+
+    redirect("/block");
+  }
   const data = await getApplicantByOpportunity(idt, token)
   return (
     <div>
-       <OporsontiyDetles job = {job} data={data}></OporsontiyDetles>
+      <OporsontiyDetles job={job} data={data}></OporsontiyDetles>
     </div>
   );
 };
